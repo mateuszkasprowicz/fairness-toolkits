30a10ffaaa653b71c64774463d21aca1a1d77044
Mateusz Kasprowicz <kasprowicz.mat@gmail.com>
2024-03-15 19:13:10 +0100

    Add constants.py file

---
+++ src/constants.py
@@ -0,0 +1,9 @@
+RANDOM_STATE = 0
+
+DEFAULT_MODEL_CONFIG = {"iterations": 3000,
+                        "depth": 3,
+                        "learning_rate": 0.01,
+                        "loss_function": "Logloss",
+                        "verbose": 250,
+                        "random_seed": RANDOM_STATE,
+                        }
